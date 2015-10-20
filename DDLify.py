750c4648f72bc950bde1a75cbffdbbf4de1d01cb
cabewaldrop <cabe.waldrop1985@gmail.com>
2015-10-20 08:13:30 -0500

    Added Instruction Docstrings to DDLify.py
    Added docstrings to DDLify.py detailing the functions that need to be
    preformed in the setup, main and teardown phases of the program.

--- DDLify.py
+++ DDLify.py
@@ -2,6 +2,10 @@ import sys
 from DDLify.utility_funcs import print_usage
 
 def setup(argv):
+    """
+    The setup function should first check and make sure that a filename or directory name has been given as a parameter.
+    It then creates blank output files for stg, owner, and amalgamation sql.
+    """
     if len(argv) != 2:
         print_usage()
         sys.exit(2)
@@ -13,10 +17,22 @@ def setup(argv):
 
 
 def main(filename):
-    print "To-Do"
+    """
+    The main function should check and see if the filename parameter represents a directory or a file.  If it is a file
+    then the file should be processed and turned into a physical model, validated, and have DDL created. If it is a
+    directory then the same process should occur for each .xls file in the directory.  If one of the files does not pass
+    validation then the job should be aborted and the error message presented to the user for correction. You will
+    probably want to investigate the os module that comes standard with python for manipulating files in order to write
+    this function.
+    """
 
 
 def teardown():
+    """
+    This is a placeholder for any cleanup work that may need to be done after the script is run. I am not sure if we will
+    need this, but it is here as part of a general pattern I like to follow with a setup, main, and teardown structure for
+    the python executable. 
+    """
     print "teardown"
 
 if __name__ == "__main__":
